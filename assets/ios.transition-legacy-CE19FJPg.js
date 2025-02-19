ec30d465ad5e0e11a9ddcbe269c9fbee55352dac
cudo <suarezrenalou@gmail.com>
2025-02-20 06:52:39 +0800

    Updates

--- assets/ios.transition-legacy-CE19FJPg.js
+++ assets/ios.transition-legacy-CE19FJPg.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CbzJH69Z.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-DK_MI0I1.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
