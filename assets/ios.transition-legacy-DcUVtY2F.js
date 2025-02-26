bd8c840bea4f211dcad40620192f20efd759b235
renalou <suarezrenalou@gmail.com>
2025-02-26 22:33:13 +0800

    Updates

--- assets/ios.transition-legacy-DcUVtY2F.js
+++ assets/ios.transition-legacy-DcUVtY2F.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CYuwzNU_.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-BtyKu7gg.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-BuqhECIb.js
+++ assets/swipe-back-legacy-BuqhECIb.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CYuwzNU_.js","./index3-legacy-J_OguyKP.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-BtyKu7gg.js","./index3-legacy-CB-XlVLm.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
