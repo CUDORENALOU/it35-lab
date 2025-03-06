bc6930b07ab9ffca7f815d73be1665d28fc33012
cudo <suarezrenalou@gmail.com>
2025-03-06 14:37:49 +0800

    Added new feature in login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,40 +1,54 @@
 import { 
+  IonAvatar,
   IonButton,
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar,
-      useIonRouter
+  IonButtons,
+    IonContent, 
+    IonHeader, 
+    IonIcon, 
+    IonInput, 
+    IonInputPasswordToggle, 
+    IonItem, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar, 
+    useIonRouter
+} from '@ionic/react';
+import { logoIonic } from 'ionicons/icons';
 
-  } from '@ionic/react';
-  
-  const Login: React.FC = () => {
-    const navigation = useIonRouter();
+const Login: React.FC = () => {
+  const navigation = useIonRouter();
 
-    const doLogin = () => {
+  const doLogin = () => {
       navigation.push('/it35-lab/app','forward','replace');
-    }
+  }
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className='ion-padding'>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }} > 
+      <IonAvatar>
+        <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />  </IonAvatar>
 
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <IonButton onClick={()=>doLogin()} expand="full">
-            Login
+      <IonItem>
+<IonInput type="password" label="Password:" value="" placeholder="Password">
+<IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+  <IonInput label="Username:" placeholder="Username" >
+</IonInput>
+  </IonInput>
+
+</IonItem>
+      </div>
+          <IonButton onClick={() => doLogin()} expand="full">
+              Login
           </IonButton>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Login;
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
