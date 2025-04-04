cc5b9af15a0dcfdbc873b6e928442e4618217680
cudo <suarezrenalou@gmail.com>
2025-04-04 08:30:46 +0800

    Updates

--- assets/md.transition-legacy-DxIUSd40.js
+++ assets/md.transition-legacy-DxIUSd40.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-is_y0j5I.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-B3boN7G-.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
