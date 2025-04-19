4c2226fd3ff1719bbfa0257fc331f8d8e612465a
Angelodatoycabana <angelodatoycabana11@gmail.com>
2025-04-19 14:10:14 +0800

    Updates

--- assets/swipe-back-legacy-BXKsOsk5.js
+++ assets/swipe-back-legacy-BXKsOsk5.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B3boN7G-.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Co5-q2pQ.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
