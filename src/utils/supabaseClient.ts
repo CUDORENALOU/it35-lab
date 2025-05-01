694e674e5bfa64b0c354b8d12115270c9413afb8
CUDORENALOU <suarezrenalou@gmail.com>
2025-05-01 09:17:42 +0800

    fix auth to place the url and key hide

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ddfgaftovuziksrbmfdm.supabase.co/';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkZmdhZnRvdnV6aWtzcmJtZmRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMjkxMTIsImV4cCI6MjA1OTkwNTExMn0.MxZJm_zijdn4NAZrN22vNvbik6Hu56WO8sPE2R7UbhU';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
