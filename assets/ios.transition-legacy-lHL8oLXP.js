3a299da2423fb70513e36fbeee218d79d1a5f650
CUDORENALOU <suarezrenalou@gmail.com>
2025-05-15 00:34:06 +0800

    Updates

--- assets/ios.transition-legacy-lHL8oLXP.js
+++ assets/ios.transition-legacy-lHL8oLXP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-gEk4grns.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CwXhohHE.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
